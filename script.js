23030f4ece679052cce4a8b760cdbbf9d77623c8
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-10-01 20:29:18 +0200

    Zrobienie dodatkowo wersji en

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function changeLanguage(lang) {
-    const cvElements = document.querySelectorAll('[data-lang]');
+  const cvElements = document.querySelectorAll("[data-lang]");
 
-    cvElements.forEach(element => {
-        const elementLang = element.getAttribute('data-lang');
+  cvElements.forEach((element) => {
+    const elementLang = element.getAttribute("data-lang");
 
-        if (elementLang === lang) {
-            element.style.display = 'block';
-        } else {
-            element.style.display = 'none';
-        }
-    });
+    if (elementLang === lang) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
 }
+
+const plButton = document.getElementById("plButton");
+const enButton = document.getElementById("enButton");
+
+plButton.addEventListener("click", function () {
+  enButton.classList.remove("active-language-button");
+  plButton.classList.add("active-language-button");
+  changeLanguage("pl");
+});
+
+enButton.addEventListener("click", function () {
+  plButton.classList.remove("active-language-button");
+  enButton.classList.add("active-language-button");
+  changeLanguage("en");
+});
